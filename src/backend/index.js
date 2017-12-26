1dd8b53ad568036a001b7a5f1bbeb6bc270fe05f
Alexandru Atanasiu <alexandru.atanasiu@outlook.com>
2017-12-26 18:07:19 +0200

    initial skeleton

---
+++ src/backend/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+
+})
+app.get('/hello', function(req, res) {
+  res.json({notes: "Hello, World, from API!"})
+});
+
+app.listen(3000);
