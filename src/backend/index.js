4a80a18c8bc6c51432eb09bc043fb0ebd2cfd413
Alexandru Atanasiu <alexandru.atanasiu@outlook.com>
2017-12-26 21:56:48 +0200

    Connected to firebase and synced simple data model

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,22 +1,45 @@
+/*
+ * 
+ */
+
+// require modules
 var express = require('express');
+var firebaseAdmin = require('firebase-admin');
 
-var app = express();
+// configure firebase (generated from Google API Dashboard)
+var serviceAccount = require("invoice-tracker-803a2656f4ed.json");
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(serviceAccount),
+  databaseURL: "https://invoice-tracker-1cc5a.firebaseio.com"
+});
 
+// create cache to database
+var database = firebaseAdmin.database();
+var ref = database.ref("/Application");
+
+var data = null;
+ref.on("value", function(snapshot) {
+  data = snapshot;
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+
+
+// configuring express
+var expressApp = express();
+expressApp.use(function(req, res, next) {
+	// add cross origin request suport
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 })
 
-app.get('/', function(req, res) {
-  res.json({notes: "Root mapping"})
+
+// api calls
+expressApp.get('/hello', function(req, res) {
+	res.json({notes2: data});
 });
 
-app.get('/hello', function(req, res) {
-  res.json({notes: "Hello, World, from API!"})
-});
-
-
-
-app.listen(3000);
+// start server listening on port 3000
+expressApp.listen(3000);
