3780287e988a46442ef5364392c9abc92dd44080
Pieptea Iulia <iulia.pieptea19@gmail.com>
2017-12-27 13:25:58 +0200

    addinvoice page

---
+++ src/frontend/js/myjs.js
@@ -0,0 +1,16 @@
+ $(document).ready(function(){
+ $("#insert-more").click(function () {
+     $(".table").each(function () {
+         var tds = '<tr>';
+         jQuery.each($('tr:last td', this), function () {
+             tds += '<td>' + $(this).html() + '</td>';
+         });
+         tds += '</tr>';
+         if ($('tbody', this).length > 0) {
+             $('tbody', this).append(tds);
+         } else {
+             $(this).append(tds);
+         }
+     });
+});
+});
